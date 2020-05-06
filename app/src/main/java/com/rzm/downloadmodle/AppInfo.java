8b16f96ea2a1e7bbb14b1f171050941e11e09715
renzhenming <1565766177@qq.com>
2020-05-06 08:57:34 +0800

    删除注释

--- app/src/main/java/com/rzm/downloadmodle/AppInfo.java
+++ app/src/main/java/com/rzm/downloadmodle/AppInfo.java
@@ -4,18 +4,8 @@ public class AppInfo {
 
     public String id;
     public String name;
-    public String packageName;
-    public String iconUrl;
-    public float stars;
-    public long size;
     public String downloadUrl;
-    public String des;
-
-
-    public String author;//黑马程序员
-    public String date;//"2015-06-10"
-    public String version;//	1.1.0605.0
-    public String downloadNum;//	40万+
+    public String version;
 
     @Override
     public String toString() {
